06fe6244b9427c89c33a9d4ed3cd4e97f0fcbfc6
4nyy <nakhsan03@gmail.com>
2023-07-31 17:13:54 +0700

    ninedh

--- app/Navbarm/Navbarm.jsx
+++ app/Navbarm/Navbarm.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import ScrollLink from '../component/Linkscroll/ScrollLink';
 import './Navbarm.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart,faRing,faImage,faCalendar} from '@fortawesome/free-solid-svg-icons';
 function Navbarm(props) {
     return (
         <nav className='Navbar-m'>
-            <a className='Navbar-click'href="#bridge">
+            <ScrollLink className='Navbar-click'href="#bridge">
                 <FontAwesomeIcon className='icon' icon={faHeart}/>
                 <p className='Navbarm-teks'>Bride & Groom</p>
-            </a>
-            <a className='Navbar-click' href="#wedding">
+            </ScrollLink>
+            <ScrollLink className='Navbar-click' href="#wedding">
                 <FontAwesomeIcon className='icon' icon={faRing}/>
                 <p className='Navbarm-teks'>Wedding</p>
-            </a>
-            <a className='Navbar-click' href="#moment">
+            </ScrollLink>
+            <ScrollLink className='Navbar-click' href="#moment">
                 <FontAwesomeIcon className='icon' icon={faImage}/>
                 <p className='Navbarm-teks'>Our Moments</p>
-            </a>
-            <a className='Navbar-click' href="">
+            </ScrollLink>
+            <ScrollLink className='Navbar-click' href="">
                 <FontAwesomeIcon className='icon' icon={faCalendar}/>
                 <p className='Navbarm-teks' >RSVP</p>
-            </a>
+            </ScrollLink>
         </nav>
     );
 }
